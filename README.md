83e9881881a98620f458226279d070fd29395dd7
BraianStrak <braianstrak1999@gmail.com>
2020-11-21 14:24:24 +0000

    Added JS module fire structure

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a todo-list web-app, part of The Odin Project curriculum!
+
+Created using webpack and bootstrap, with local storage on firebase.
--- src/index.js
+++
@@ -1,7 +0,0 @@
-console.log("hi!");
-
-let content = document.getElementById('content');
-let redParagraph = document.createElement("p");
-redParagraph.style.color="red";
-redParagraph.textContent="Hey I am Red!";
-content.appendChild(redParagraph);
