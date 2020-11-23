9c88541bd1425bb75ce22d0041245a0a05b38703
BraianStrak <braianstrak1999@gmail.com>
2020-11-23 18:58:43 +0000

    List switching implemented

--- src/index.js
+++ src/index.js
@@ -10,26 +10,69 @@ addItemBtn.addEventListener('click', addItem);
 var addListBtn = document.querySelector('#addListBtn');
 addListBtn.addEventListener('click', addList);
 
+let lists = [];
+let listButtons = [];
+let currentListIndex = 0;
 
 //the current todo list we are adding stuff to, by default it is the default one
-let currentList = todoListFactory("default");
-renderList(currentList.content);
+let defaultList = todoListFactory("default");
+renderList(defaultList.content);
+lists.push(defaultList);
+
+let listContainer = document.getElementById('todo-lists-container');
+let defaultListButton = listContainer.lastElementChild;
+listButtons.push(defaultListButton);
+initialiseListButton(defaultListButton, 0);
+
 
 function addItem() {
     //maybe this should be adding exclusively to a list
     const thing = todoItemFactory(document.getElementById("todoItemName").value);
     document.getElementById('todoItemName').value = todoItemName.defaultValue;
 
-    currentList.addItem(thing);
+    //this works
+    lists[currentListIndex].addItem(thing);
 
     clearAllItems();
-    renderAllItems(currentList.items);
+
+    //this works
+    renderAllItems(lists[currentListIndex].items);
 }
 
 function addList() {
     const thing = todoListFactory(document.getElementById("todoListName").value);
     renderList(thing.content);
+    lists.push(thing);
     document.getElementById('todoListName').value = todoListName.defaultValue; 
+
+    //thanks to this, the list button will be the same index as the list on both arrays
+
+    let listButton = listContainer.lastElementChild;
+    listButtons.push(listButton);
+
+    switchList(lists.length-1);
+    initialiseListButton(listButton, lists.length-1);
 }
 
 
+function switchList(listIndex) {
+    console.log("i am switching list to " + listIndex)
+    currentListIndex = listIndex; //one of these is null for some reason 
+    clearAllItems();
+    renderAllItems(lists[currentListIndex].items);
+}
+
+//when the button is clicked, switch to that list
+function initialiseListButton(button, index){
+    console.log("button being initialised");
+
+    //hack to add a listener to a dynamically created button
+    button.addEventListener('click',function(e){
+        if(e.target && e.target.id== 'dynamicButton'){
+              //do something
+              button.addEventListener('click', switchList(index));
+         }
+    });
+}
+
+//it calls switch then init then switch again whenever I create a new list, find out why.
--- src/modules/todoListRenderer.js
+++ src/modules/todoListRenderer.js
@@ -2,6 +2,7 @@ const renderList = (content) => {
     let newList = document.createElement('button');
     newList.textContent = content;
     newList.classList.add("d-block", "todoText", "w-100", "bg-secondary", "mb-2");
+    newList.id = "dynamicButton";
 
     let listContainer = document.getElementById('todo-lists-container');
     listContainer.appendChild(newList);
